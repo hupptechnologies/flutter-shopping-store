830d5089e62f3c2491e11a308bf3def4caacc0bb
hupptechnologies <git@hupp.in>
2025-02-10 16:55:18 +0530

    UI - notification setting screen

--- lib/screens/appSetting/screens/notification/controller/setting_notification_controller.dart
+++ lib/screens/appSetting/screens/notification/controller/setting_notification_controller.dart
@@ -1,3 +1,32 @@
 import 'package:get/get.dart';
 
-class SettingNotificationController extends GetxController {}
+class SettingNotificationController extends GetxController {
+  final RxList list = [
+    {
+      "id": 1,
+      "title": "Show notifications",
+      "subTitle": "Receive push notifications for new messages",
+      "value": true,
+    },
+    {
+      "id": 2,
+      "title": "Notification sounds",
+      "subTitle": "Play sound for new messages",
+      "value": true,
+    },
+    {
+      "id": 3,
+      "title": "Lock screen notifications",
+      "subTitle": "Allow notification on the lock screen",
+      "value": false,
+    }
+  ].obs;
+
+  void onChangeValue(int id, bool value) {
+    final index = list.indexWhere((element) => element["id"] == id);
+    if (index != -1) {
+      list[index]["value"] = value;
+      list.refresh();
+    }
+  }
+}
--- lib/screens/appSetting/screens/notification/view/setting_notification_view.dart
+++ lib/screens/appSetting/screens/notification/view/setting_notification_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/common/constant/app_colors.dart';
 import 'package:e_commerce/common/constant/margin_padding.dart';
 import 'package:e_commerce/screens/appSetting/screens/notification/controller/setting_notification_controller.dart';
 import 'package:e_commerce/widgets/back_button_appbar_title.dart';
@@ -14,7 +15,7 @@ class SettingNotificationView extends GetView<SettingNotificationController> {
       child: Scaffold(
         appBar: const BackButtonAppbarTitle(
           centerTitle: true,
-          title: 'Setting Notification',
+          title: 'Notification',
         ),
         body: SafeArea(
           child: Padding(
@@ -22,8 +23,36 @@ class SettingNotificationView extends GetView<SettingNotificationController> {
               horizontal: MarginPadding.homeHorPadding,
               vertical: MarginPadding.homeTopPadding,
             ),
-            child: const Center(
-              child: Text('Setting Notification'),
+            child: Obx(
+              () => ListView.builder(
+                itemCount: controller.list.length,
+                itemBuilder: (context, index) {
+                  final item = controller.list[index];
+
+                  return ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: Text(
+                        item['title'],
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      subtitle: Text(
+                        item['subTitle'],
+                        style: const TextStyle(fontSize: 10),
+                      ),
+                      trailing: Transform.scale(
+                        scale: 0.8,
+                        child: Switch(
+                          value: item['value'],
+                          activeTrackColor: AppColors.lightGreen,
+                          onChanged: (value) =>
+                              controller.onChangeValue(item['id'], value),
+                        ),
+                      ));
+                },
+              ),
             ),
           ),
         ),
