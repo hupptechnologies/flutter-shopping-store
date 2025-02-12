e0ddbe638eb4670413e1de1e4ce45bb98a1a30f4
hupptechnologies <git@hupp.in>
2025-02-12 14:00:46 +0530

    UI - language screen

--- lib/screens/appSetting/screens/language/controller/language_controller.dart
+++ lib/screens/appSetting/screens/language/controller/language_controller.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
 
 class LanguageController extends GetxController {
-  @override
-  void onInit() {
-    print(Get.currentRoute);
-    super.onInit();
+  final List<String> languages = [
+    'English',
+    'हिन्दी',
+    'ગુજરાતી',
+  ];
+
+  late Rx<String> selectedLan = 'English'.obs;
+
+  void changeLanguage(String? value) {
+    if (value != null && value.isNotEmpty) {
+      selectedLan.value = value;
+    }
   }
 }
--- lib/screens/appSetting/screens/language/view/language_view.dart
+++ lib/screens/appSetting/screens/language/view/language_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/common/constant/app_colors.dart';
 import 'package:e_commerce/common/constant/margin_padding.dart';
 import 'package:e_commerce/screens/appSetting/screens/language/controller/language_controller.dart';
 import 'package:e_commerce/widgets/back_button_appbar_title.dart';
@@ -14,7 +15,7 @@ class LanguageView extends GetView<LanguageController> {
       child: Scaffold(
         appBar: const BackButtonAppbarTitle(
           centerTitle: true,
-          title: 'Language',
+          title: 'Languages',
         ),
         body: SafeArea(
           child: Padding(
@@ -22,8 +23,38 @@ class LanguageView extends GetView<LanguageController> {
               horizontal: MarginPadding.homeHorPadding,
               vertical: MarginPadding.homeTopPadding,
             ),
-            child: const Center(
-              child: Text('Language'),
+            child: ListView(
+              children: [
+                ListView.builder(
+                  itemCount: controller.languages.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    final item = controller.languages[index];
+                    return Obx(
+                      () => Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 6),
+                        child: ListTile(
+                          onTap: () => controller.changeLanguage(item),
+                          title: Text(item),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                          leading: Radio<String>(
+                            visualDensity: const VisualDensity(horizontal: -4),
+                            value: item,
+                            groupValue: controller.selectedLan.value,
+                            onChanged: controller.changeLanguage,
+                          ),
+                          tileColor: AppColors.lightGray,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ),
