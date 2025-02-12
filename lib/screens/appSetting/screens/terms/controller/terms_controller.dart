5b54deda5a2fdfe8767ddd66be40c86c4d23dc01
hupptechnologies <git@hupp.in>
2025-02-12 10:18:02 +0530

    UI - Our Terms Of Service screen

--- lib/screens/appSetting/screens/terms/controller/terms_controller.dart
+++ lib/screens/appSetting/screens/terms/controller/terms_controller.dart
@@ -1,3 +1,60 @@
 import 'package:get/get.dart';
 
-class TermsController extends GetxController {}
+class TermsController extends GetxController {
+  final List<Map<String, String>> terms = [
+    {
+      "title": "Acceptance of Terms",
+      "subtitle":
+          "By using the app, users agree to these Terms of Use and any updates. Continued use of the app after changes signifies acceptance."
+    },
+    {
+      "title": "User Accounts & Responsibilities",
+      "subtitle":
+          "Users must provide accurate registration details and keep their login credentials secure. The app reserves the right to suspend or terminate accounts for violations."
+    },
+    {
+      "title": "Product Listings & Pricing",
+      "subtitle":
+          "The app strives to provide accurate product descriptions and pricing. However, errors may occur, and the app reserves the right to correct them or cancel affected orders."
+    },
+    {
+      "title": "Orders, Payments & Billing",
+      "subtitle":
+          "All purchases are subject to availability and payment verification. Users must provide valid payment details and authorize the app to charge the total amount."
+    },
+    {
+      "title": "Shipping & Delivery",
+      "subtitle":
+          "Delivery times are estimates and may vary. The app is not responsible for delays caused by third-party carriers or incorrect shipping information provided by users."
+    },
+    {
+      "title": "Returns, Refunds & Cancellations",
+      "subtitle":
+          "Users can request returns and refunds per the app’s policy. Some items may not be eligible for returns, and refund processing times may vary."
+    },
+    {
+      "title": "Prohibited Activities",
+      "subtitle":
+          "Users must not engage in fraud, abuse, hacking, unauthorized reselling, or activities that violate laws. Violation may lead to account termination and legal action."
+    },
+    {
+      "title": "Intellectual Property",
+      "subtitle":
+          "All content, including logos, trademarks, and product images, belongs to the app or respective owners. Users may not copy, modify, or distribute app content without permission."
+    },
+    {
+      "title": "Limitation of Liability",
+      "subtitle":
+          "The app is not liable for direct, indirect, or incidental damages resulting from app usage, including but not limited to product issues, delays, or service interruptions."
+    },
+    {
+      "title": "Privacy & Data Protection",
+      "subtitle":
+          "The app collects and processes user data according to its Privacy Policy. Users consent to the collection and use of their data as outlined in the policy."
+    }
+  ];
+
+  void acceptTap() {
+    Get.back();
+  }
+}
--- lib/screens/appSetting/screens/terms/view/terms_view.dart
+++ lib/screens/appSetting/screens/terms/view/terms_view.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce/common/constant/margin_padding.dart';
 import 'package:e_commerce/screens/appSetting/screens/terms/controller/terms_controller.dart';
 import 'package:e_commerce/widgets/back_button_appbar_title.dart';
+import 'package:e_commerce/widgets/button_widget.dart';
 import 'package:e_commerce/widgets/pop_scope_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,7 +15,7 @@ class TermsView extends GetView<TermsController> {
       child: Scaffold(
         appBar: const BackButtonAppbarTitle(
           centerTitle: true,
-          title: 'Terms',
+          title: 'Our Terms Of Service',
         ),
         body: SafeArea(
           child: Padding(
@@ -22,8 +23,45 @@ class TermsView extends GetView<TermsController> {
               horizontal: MarginPadding.homeHorPadding,
               vertical: MarginPadding.homeTopPadding,
             ),
-            child: const Center(
-              child: Text('Terms'),
+            child: SingleChildScrollView(
+              child: Column(
+                spacing: 15,
+                children: [
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: controller.terms.length,
+                    itemBuilder: (context, index) {
+                      final item = controller.terms[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          spacing: 6,
+                          children: [
+                            Text(
+                              '${(index + 1).toString()} ${item['title']}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(item['subtitle']!),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  FractionallySizedBox(
+                    widthFactor: 0.7,
+                    child: ButtonWidget(
+                      title: 'Accept & Continue',
+                      isDisable: false,
+                      onPressed: controller.acceptTap,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
