9d085718ec0c884600aff77ca9e11dbfd152fb56
hupptechnologies <git@hupp.in>
2025-02-12 15:32:41 +0530

    Config eslint or prettierrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,31 +5,56 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+	{
+		ignores: ['eslint.config.mjs'],
+	},
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	eslintPluginPrettierRecommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+			ecmaVersion: 5,
+			sourceType: 'module',
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-floating-promises': 'warn',
+			'@typescript-eslint/no-unsafe-argument': 'warn',
+			'@typescript-eslint/interface-name-prefix': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'error',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			indent: ['error', 'tab', { SwitchCase: 1 }],
+			'prettier/prettier': ['error', { useTabs: true, tabWidth: 4 }],
+			'max-len': ['error', { code: 250, ignoreComments: true, ignoreTrailingComments: true }],
+			'@typescript-eslint/no-unused-vars': 'error',
+			'object-curly-newline': [
+				'error',
+				{
+					ObjectExpression: 'always',
+					ObjectPattern: { multiline: true, minProperties: 8 },
+				},
+			],
+			'object-curly-spacing': ['error', 'always'],
+			'object-property-newline': 'error',
+			'object-shorthand': 'error',
+			'comma-spacing': ['error', { before: false, after: true }],
+			'space-in-parens': ['error', 'never'],
+			'space-infix-ops': 'error',
+			'space-unary-ops': 'error',
+			'spaced-comment': ['error', 'always'],
+			'no-trailing-spaces': 'error',
+			'no-whitespace-before-property': 'error',
+			'eol-last': ['error', 'always'],
+		},
+	},
 );
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, Logger, RequestMethod } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+class Application {
+	private readonly logger = new Logger(Application.name);
+
+	public async run(): Promise<void> {
+		try {
+			const app: INestApplication<AppModule> = await NestFactory.create(AppModule);
+
+			const port = this.configureApp(app);
+			await app.listen(port);
+
+			this.logger.log(`Application is running on: http://localhost:${port}`);
+		} catch (error) {
+			this.logger.error('Error during application bootstrap:', error);
+		}
+	}
+
+	private configureApp(app: INestApplication<AppModule>): number {
+		const configService = app.get(ConfigService);
+
+		const prefix = configService.get<string>('PREFIX') ?? '';
+
+		app.setGlobalPrefix(prefix, {
+			exclude: [
+				{
+					path: '/',
+					method: RequestMethod.GET,
+				},
+			],
+		});
+		const port = configService.get<number>('PORT') ?? 3000;
+
+		return port;
+	}
 }
-bootstrap();
+
+const application = new Application();
+void application.run();
