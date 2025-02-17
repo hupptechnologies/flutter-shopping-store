1686ed32f28ed1d4666a688914ec62edb368d621
hupptechnologies <git@hupp.in>
2025-02-17 12:10:30 +0530

    The email template for successfully resetting the password has been added

--- src/common/constant/mail-subject.constant.ts
+++ src/common/constant/mail-subject.constant.ts
@@ -1,4 +1,5 @@
 export abstract class MailSubjectConstant {
 	static readonly REGISTRATION = 'Welcome to Shopping store';
 	static readonly OTP = 'Shopping store OTP Verification Code';
+	static readonly RESET_PASSWORD_SUCCESS = 'Your Password Has Been Reset Successfully';
 }
--- src/common/constant/template.constant.ts
+++ src/common/constant/template.constant.ts
@@ -1,4 +1,5 @@
 export abstract class TemplateConstant {
 	static readonly REGISTRATION = 'registration';
 	static readonly OTP = 'otp';
+	static readonly RESET_PASSWORD_SUCCESS = 'reset-password-success';
 }
