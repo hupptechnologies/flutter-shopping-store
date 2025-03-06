75370659f954a67014aa7782cb04168efb8381bc
hupptechnologies <git@hupp.in>
2025-03-06 11:34:58 +0530

    resolve the issue of empty error exception messages

--- src/common/constant/error-msg.constant.ts
+++ src/common/constant/error-msg.constant.ts
@@ -4,6 +4,7 @@ export abstract class ErrorMsgConstant {
 	static readonly SOMETHING_WRONG = 'Something went wrong. Please try again.';
 	static readonly INVALID_OTP = 'Invalid OTP';
 	static readonly OTP_EXPIRED = 'OTP expired';
-	static readonly OTP_NOT_VERIFIED: 'OTP has not been verified. Please verify your OTP first.';
-	static readonly SAME_PASSWORD_NOT_ALLOWED: 'You cannot use your previous password as your new password.';
+	static readonly OTP_NOT_VERIFIED = 'OTP has not been verified. Please verify your OTP first.';
+	static readonly SAME_PASSWORD_NOT_ALLOWED =
+		'You cannot use your previous password as your new password.';
 }
