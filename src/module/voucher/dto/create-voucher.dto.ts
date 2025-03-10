072bcefa66a9a0870fab77be12b03c5fa7421ac9
hupptechnologies <git@hupp.in>
2025-03-10 12:37:37 +0530

    Added the create voucher DTO with validation

---
+++ src/module/voucher/dto/create-voucher.dto.ts
@@ -0,0 +1,43 @@
+import { IsEnum, IsNumber, IsOptional, Min } from 'class-validator';
+import { DiscountEnum } from 'src/common/enum/discount.enum';
+import { IsNotEmpty } from 'src/decorator/validation/is-not-empty.decorator';
+
+export class CreateVoucherDto {
+	@IsNotEmpty()
+	public code: string;
+
+	@IsEnum(DiscountEnum)
+	public discountType: DiscountEnum;
+
+	@IsNumber()
+	@Min(0)
+	public discountValue: number;
+
+	@IsOptional()
+	@IsNumber()
+	@Min(0)
+	public minOrderValue?: number;
+
+	@IsOptional()
+	@IsNumber()
+	@Min(0)
+	public maxDiscount?: number;
+
+	@IsOptional()
+	public validFrom?: Date;
+
+	@IsOptional()
+	public validTo?: Date;
+
+	@IsNumber()
+	@Min(1)
+	public usageLimit: number;
+
+	@IsOptional()
+	@IsNumber()
+	@Min(0)
+	public usedCount?: number;
+
+	@IsOptional()
+	public userSpecific?: boolean;
+}
--- src/module/voucher/voucher.service.ts
+++ src/module/voucher/voucher.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { Loggable } from 'src/decorator/loggable/loggable.decorator';
 
 @Injectable()
+@Loggable()
 export class VoucherService {}
