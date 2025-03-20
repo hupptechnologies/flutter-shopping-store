c4bb28a010ad536842544d4ee7548326f47d6c0b
hupptechnologies <git@hupp.in>
2025-03-20 12:09:30 +0530

    Integrate the image DTO class

---
+++ lib/dto/image_dto.dart
@@ -0,0 +1,15 @@
+List<ImageDto> imageDtoFromJson(List<dynamic> jsonList) {
+  return jsonList.map((json) => ImageDto.fromJson(json)).toList();
+}
+
+class ImageDto {
+  int? id;
+  String? url;
+
+  ImageDto({this.id, this.url});
+
+  factory ImageDto.fromJson(Map<String, dynamic> json) => ImageDto(
+        id: json['id'],
+        url: json['url'],
+      );
+}
