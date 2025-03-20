e0d37de4010dcd13e681ccb2bc2cbbb315030124
hupptechnologies <git@hupp.in>
2025-03-20 16:16:31 +0530

    Search sidcover screen in Update category card UI

--- lib/screens/home/view/widget/search_discover_sub_category.dart
+++ lib/screens/home/view/widget/search_discover_sub_category.dart
@@ -26,50 +26,41 @@ class SearchDiscoverSubCategory extends GetView<SearchDiscoverController> {
           final hasSubCategory = item.children?.isNotEmpty ?? false;
           return Column(
             children: [
-              GestureDetector(
+              ListTile(
                 onTap: () => controller.onTapProduct(item.name!),
-                child: Row(
+                contentPadding: EdgeInsets.only(left: isSubCategory ? 20 : 0),
+                visualDensity:
+                    const VisualDensity(horizontal: -4, vertical: -4),
+                title: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    // Text on the left side
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: isSubCategory ? 25 : 16.0,
-                        right: 16.0,
-                        top: 10,
-                        bottom: 10,
-                      ),
-                      child: Text(
-                        item.name!,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          overflow: TextOverflow.ellipsis,
-                        ),
+                    Text(
+                      item.name!,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          '${item.productCount!} Items',
-                          style: TextStyle(
-                            color: AppColors.darkGray.withOpacityValue(0.6),
-                          ),
-                        ),
-                        const SizedBox(width: 5),
-                        const Icon(
-                          Icons.arrow_forward_ios,
-                          color: AppColors.darkGray,
-                          size: 15,
-                        ),
-                      ],
-                    )
+                    Text(
+                      '${item.productCount!} Items',
+                      style: TextStyle(
+                        color: AppColors.darkGray.withOpacityValue(0.6),
+                      ),
+                    ),
                   ],
+                ),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColors.darkGray,
+                  size: 15,
                 ),
               ),
               if (hasSubCategory) ...[
                 const Divider(thickness: 1.0, color: AppColors.lightGray),
                 SearchDiscoverSubCategory(
-                    categories: item.children!, isSubCategory: true),
+                  categories: item.children!,
+                  isSubCategory: true,
+                ),
               ],
               if (categories.last != item)
                 const Divider(thickness: 1.0, color: AppColors.lightGray),
