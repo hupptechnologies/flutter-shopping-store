119241c47e35f57f16c25d86925555af90cc7858
hupptechnologies <git@hupp.in>
2025-04-10 10:22:00 +0530

    Remove unused import from review section widget

--- lib/screens/product/view/widgets/reivewSection/review_section_users_reviews.dart
+++ lib/screens/product/view/widgets/reivewSection/review_section_users_reviews.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce/common/constant/image_constant.dart';
 import 'package:e_commerce/screens/product/controller/product_detail_controller.dart';
 import 'package:e_commerce/widgets/review_card_widget.dart';
 import 'package:flutter/material.dart';
